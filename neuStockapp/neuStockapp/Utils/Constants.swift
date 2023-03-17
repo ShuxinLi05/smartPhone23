85fbc7a1eb4899b270822f4a619dc1cee1067669
susie <shuxinli0512@outlook.com>
2023-03-16 22:05:17 -0700

    check neustock app again
    check neustock app again

---
+++ neuStockapp/neuStockapp/Utils/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  neuStockapp
+//
+//  Created by shuxin on 3/16/23.
+//
+
+import Foundation
+let apiKey = "f468dbda8be0185b0a65634da024b7f7"
+let baseURL = "https://financialmodelingprep.com/api/v3/quote-short/"
